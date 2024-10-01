54df8623c3b1faa82767ee0ce091ae713ea3e28f
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-10-01 20:38:08 +0300

    Add fetch logic from the api

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react"
+import styles from './Shop.module.css'
+
 export default function Shop() {
+  const [categories, setCategories] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchDataForCategories = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products/categories', { mode: "cors" })
+        if (!response.ok) {
+          throw new Error(`HTTP error: Status ${response.status}`);
+        }
+        let categoriesData = await response.json();
+        setCategories(categoriesData);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setCategories(null);
+      } finally {
+        setLoading(false);
+      }
+
+    };
+    
+    fetchDataForCategories();
+  }, [])
+
   return (
-    <div>shop</div>
+    <div>
+      {loading && <p>Loading</p>}
+      {
+        categories && 
+        <ul>
+          {categories.map((category, index) => 
+          <li key={index}>{category}</li>)}
+        </ul>
+      }
+    </div>
   )
 }
